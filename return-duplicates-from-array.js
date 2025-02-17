f6b816aee502ccecf23203b2ab5c2c16ebbeadec
Varun Pandey <65996450+varundwt@users.noreply.github.com>
2025-02-17 14:44:34 +0530

    Update return-duplicates-from-array.js

--- return-duplicates-from-array.js
+++ return-duplicates-from-array.js
@@ -5,3 +5,20 @@ console.log([...new Set(fruits)]);
 //return duplicates only
 const sort = [...new Set(fruits.filter((item, i, arr)=>arr.indexOf(item)!==i)) ]
 console.log(sort);
+
+
+//without using set method:
+
+array = [3, 7, 2, 9, 3, 5, 7, 8, 2, 4];
+
+function remove(arr){
+  let unique = [];
+  for (let i = 0; i < arr.length; i++) {
+    if(!unique.includes(arr[i])){
+      unique.push(arr[i])
+    }       
+  }
+  return unique;
+}
+
+console.log(remove(array))
