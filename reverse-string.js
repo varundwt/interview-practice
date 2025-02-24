765f9882c3e08ea1c6cb8c856c54e70d1d72b901
Varun Pandey <65996450+varundwt@users.noreply.github.com>
2025-02-24 11:55:51 +0530

    Update reverse-string.js

--- reverse-string.js
+++ reverse-string.js
@@ -4,3 +4,17 @@ function vowels(str) {
 }
 
 vowels("varun")
+
+//without using reverse
+
+const abc = [1,2,3,4,5];
+
+function rev(arr){
+  const ww = []
+  for (var i = arr.length -1 ; i>=0; i-- ) {
+    ww.push(arr[i])
+  }
+  console.log(ww)
+}
+
+rev(abc);
