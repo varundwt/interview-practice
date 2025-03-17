5ea8c6a04b8fc85d61026cdd6f7bca59b0c31bc0
Varun Pandey <65996450+varundwt@users.noreply.github.com>
2025-03-17 19:41:27 +0530

    Update filter-item-from-json.jsx

--- react/filter-item-from-json.jsx
+++ react/filter-item-from-json.jsx
@@ -1,31 +1,35 @@
 import "./styles.css";
-import React, { useState } from "react";
-const data = [
-    { id: 1, name: "Apple", category: "Fruit" },
-    { id: 2, name: "Carrot", category: "Vegetable" },
-    { id: 3, name: "Banana", category: "Fruit" },
-    { id: 4, name: "Broccoli", category: "Vegetable" },
-    { id: 5, name: "Strawberry", category: "Fruit" }
-  ];
-export default function App() {
-  const [searchTerm, setSearchTerm] = useState("");
+import React, { useState, useEffect } from "react";
 
-  const filteredData = data.filter(item =>
-    item.name.toLowerCase().includes(searchTerm.toLowerCase())
+export default function App() {
+  const [apiData, setApiData] = useState([]);
+  const [searched, setSearched] = useState("");
+
+  const fetchData = async () => {
+    const data = await fetch("https://dummyjson.com/products");
+    const dataJSON = await data.json();
+    setApiData(dataJSON?.products);
+    console.log(apiData);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const filteredData = apiData.filter((item) =>
+    item.title.toLowerCase().includes(searched.toLowerCase())
   );
 
   return (
-    <div style={{ padding: "20px", maxWidth: "400px", margin: "auto" }}>
+    <div>
       <input
         type="text"
-        placeholder="Search by name..."
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        style={{ width: "100%", padding: "8px", marginBottom: "10px" }}
+        value={searched}
+        onChange={(e) => setSearched(e.target.value)}
       />
       <ul>
-        {filteredData.map(item => (
-          <li key={item.id}>{item.name} - {item.category}</li>
+        {filteredData.map((item) => (
+          <li key={item.id}>{item.title}</li>
         ))}
       </ul>
     </div>
